2ae6f8423023447b12b8177641643b417e4c903d
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-11-29 14:18:44 +0530

    cors error

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
-const cors = require('cors');
+// const cors = require('cors');
 
-server.use(cors({
- origin:"https://hungry-ruby-trunks.cyclic.app/books"
-}))
+// server.use(cors({
+//  origin:"https://hungry-ruby-trunks.cyclic.app/books",
+//  "Access-Control-Allow-Credentials": true
+// }))
  
 server.use(middlewares);
 server.use(router);
